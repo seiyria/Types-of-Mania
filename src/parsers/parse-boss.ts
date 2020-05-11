7af922f9d138cac1badf337d463e9585fecac7fb
Kyle J. Kemp <kyle@seiyria.com>
2020-05-11 14:00:26 -0500

    make chardata -> charadata for consistency

--- src/parsers/parse-boss.ts
+++ src/parsers/parse-boss.ts
@@ -6,6 +6,6 @@ export const BossParsers = [new EnemyParser({
   offsetStart: 140,
   offsetIteratorIncrementBy: 1624,
   type: EnemyType.Boss,
-  uexpFilePath: `gamefiles/boss/uexp/chardata/BossStatusTable.uexp`,
-  jsonFilePath: `gamefiles/boss/json/chardata/BossStatusTable.json`
+  uexpFilePath: `gamefiles/boss/uexp/charadata/BossStatusTable.uexp`,
+  jsonFilePath: `gamefiles/boss/json/charadata/BossStatusTable.json`
 })];
